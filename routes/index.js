797285ad20a8af9a1f165019efedb298175b2bf7
Nicu Farmache <gitlab@nicu.ro>
2021-10-07 16:25:18 +0200

    Fixed power button

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,11 @@ var express = require('express');
 var router = express.Router();
 
 var svg = fs.readFileSync('./public/images/Dell_Power_Connect_3524P_switch.svg', 'utf8');
+var power = fs.readFileSync('./public/images/power.svg', 'utf8');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { svg });
+  res.render('index', { svg, power });
 });
 
 module.exports = router;
